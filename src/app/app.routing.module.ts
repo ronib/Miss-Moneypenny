b9b72dcab509e4f597dd11d591ef181d22cf6b1e
Felix Piskunov <joylandraver@gmail.com>
2018-10-24 22:28:07 +0300

    Added home route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SettingsComponent } from './settings/settings.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: ChatbotComponent
+        component: HomeComponent
     },
     {
-        path: 'settings',
-        component: SettingsComponent
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+            {
+                path: '',
+                component: ChatbotComponent
+            },
+            {
+                path: 'settings',
+                component: SettingsComponent
+            }
+        ]
     }
 ];
 
