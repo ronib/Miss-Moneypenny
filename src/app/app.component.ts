b968611a83e4a16bf56c8641f834885271bc5d66
alonav <avgalon@gmail.com>
2018-10-24 04:49:58 -0700

    new property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   constructor(){
     this.message = new Message('', 'assets/images/user.png');
     this.messages = [
-      new Message('Welcome to chatbot universe', 'assets/images/bot.png', new Date())
+      new Message('Welcome to chatbot universe', 'assets/images/bot.png', new Date(), true)
     ];
   }
 }
--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -2,10 +2,12 @@ export class Message {
   content: string;
   timestamp: Date;
   avatar: string;
+  isRobot = false;
 
-  constructor(content: string, avatar: string, timestamp?: Date){
+  constructor(content: string, avatar: string, timestamp?: Date, isRobot?: boolean) {
     this.content = content;
     this.timestamp = timestamp;
     this.avatar = avatar;
+    this.isRobot = isRobot;
   }
 }
